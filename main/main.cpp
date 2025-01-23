607ad165d060116ef0512e5698615b216dbd92d3
Martijn Faber <martijn@fabtronics.nl>
2025-01-23 20:16:44 +0000

    fix: fix tx_queue latency issues

--- main/main.cpp
+++ main/main.cpp
@@ -17,17 +17,20 @@
 #include <algorithm>
 #include "sensor.h"
 
-const size_t BUF_SIZE = 256;
-const size_t ECHO_TASK_STACK_SIZE = 4096;
-const size_t MAX_MESSAGE_LEN = 256;
-const size_t SAMPLE_RATE = 10;
+// 9,28 cm/s
+
+static const char* TAG = "Main";
+
+const size_t BUF_SIZE = 128;
+const size_t MAX_MESSAGE_LEN = 128;
+const size_t SAMPLE_RATE = 20;
 const size_t SAMPLE_INTERVAL_MS = 1000 / SAMPLE_RATE;
 
 struct SerialTaskParams
 {
     QueueHandle_t tx_queue;
     QueueHandle_t rx_queue;
-};
+}; 
 
 
 struct SensorResult
@@ -93,7 +96,7 @@ void sampler(void* parameters)
 
     SamplerQueueItem queue_item;
 
-    while (1) {
+    while (true) {
         memset(&queue_item, 0, sizeof(queue_item));
         queue_item.timestamp = esp_timer_get_time();
 
@@ -117,6 +120,9 @@ void serial_task(void* parameters)
 {
     auto params = static_cast<SerialTaskParams*>(parameters);
 
+    int counter = 0;
+    int64_t last_update = 0;
+
     char tx_buf[MAX_MESSAGE_LEN];
     memset(tx_buf, 0, sizeof(tx_buf));
     char rx_buf[MAX_MESSAGE_LEN];
@@ -127,7 +133,9 @@ void serial_task(void* parameters)
 
     while (true)
     {
-        if (xQueueReceive(params->tx_queue, tx_buf, portMAX_DELAY)) {
+        // While loop to empty the tx queue
+        while (xQueueReceive(params->tx_queue, tx_buf, pdMS_TO_TICKS(1))) {
+            ESP_LOGI(TAG, "Queue receive item from tx queue");
             usb_serial_jtag_write_bytes(tx_buf, strlen(tx_buf), 20 / portTICK_PERIOD_MS);
             memset(tx_buf, 0, sizeof(tx_buf));
         }
@@ -136,7 +144,6 @@ void serial_task(void* parameters)
 
         // Write data back to the USB SERIAL JTAG
         if (len > 0) {
-
             // check if we have enough space in rd_buf
             if ((rd_len + len + 1)  < sizeof (rx_buf)) {
                 // copy in new received data
@@ -169,6 +176,15 @@ void serial_task(void* parameters)
                         size_t olen = (size_t)end - (size_t)rx_buf;
                         rx_buf[olen] = '\0';
 
+                        counter++;
+
+                        // if ((esp_timer_get_time() - last_update) >= 1000000)
+                        // {
+                        //     ESP_LOGI(TAG, "Counter: %d", counter);
+                        //     counter = 0;
+                        //     last_update = esp_timer_get_time();
+                        // }
+
                         xQueueSend(params->rx_queue, &rx_buf[1], portMAX_DELAY);
                         
                         rd_len = 0;
@@ -178,7 +194,8 @@ void serial_task(void* parameters)
             }
         }
 
-        vTaskDelay(pdMS_TO_TICKS(1));
+        // ESP_LOGI(TAG, "Serial task loop\n");
+        // vTaskDelay(pdMS_TO_TICKS(1));
     }
 }
 
@@ -187,6 +204,7 @@ void serial_task(void* parameters)
 // 4 bytes because command consists of 3 characters + zero terminator string
 void send_message(const QueueHandle_t queue, const char command[4], const char* payload)
 {
+    // ESP_LOGI(TAG, "Send message\n");
     static char buf[MAX_MESSAGE_LEN];
 
     memset(buf, 0x00, sizeof(buf));
@@ -211,6 +229,28 @@ void send_heartbeat(const QueueHandle_t queue)
 }
 
 void send_sensor_measurements(const QueueHandle_t queue, const SamplerQueueItem& item)
+{
+    static char payload[MAX_MESSAGE_LEN];
+
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+        memset(payload, 0x00, sizeof(payload));
+
+        auto sensor = item.results[i];
+        snprintf(payload, sizeof(payload), "T=%lld:ID=%d:C=%d:V=%d",
+            item.timestamp, 
+            sensor.id,
+            sensor.connected,
+            sensor.value);
+        
+        // ESP_LOGI(TAG, "Sending message for sensor: %d", i+1);
+        send_message(queue, "SMS", payload);
+    }
+
+    // TODO: naam
+}
+
+void send_dimensions(const QueueHandle_t queue, const SamplerQueueItem& item)
 {
     static char payload[MAX_MESSAGE_LEN];
     memset(payload, 0x00, sizeof(payload));
@@ -252,14 +292,14 @@ extern "C" void app_main(void)
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&unit_config, &adc1_handle));
 
     Sensor sensors[NUM_SENSORS] = {
-        Sensor(1, 1300, adc1_handle, ADC_CHANNEL_5, GPIO_NUM_41, false, 60, 0),
-        Sensor(2, 1300, adc1_handle, ADC_CHANNEL_4, GPIO_NUM_42, true, 120, 50),
-        Sensor(3, 1300, adc1_handle, ADC_CHANNEL_3, GPIO_NUM_2, false, 180, 0),
-        Sensor(4, 1300, adc1_handle, ADC_CHANNEL_6, GPIO_NUM_1, true, 240, 0),
-        Sensor(5, 1300, adc1_handle, ADC_CHANNEL_7, GPIO_NUM_21, false, 300, 0),
-        Sensor(6, 1300, adc1_handle, ADC_CHANNEL_9, GPIO_NUM_47, true, 360, 0),
-        Sensor(7, 1300, adc1_handle, ADC_CHANNEL_8, GPIO_NUM_48, false, 420, 0),
-        Sensor(8, 1300, adc1_handle, ADC_CHANNEL_2, GPIO_NUM_45, true, 480, 0)
+        Sensor(1, 1300, adc1_handle, ADC_CHANNEL_5, GPIO_NUM_41, true, 0, 0),
+        Sensor(2, 1300, adc1_handle, ADC_CHANNEL_4, GPIO_NUM_42, false, 800, 0),
+        Sensor(3, 1300, adc1_handle, ADC_CHANNEL_3, GPIO_NUM_2, true, 1600, 0),
+        Sensor(4, 1300, adc1_handle, ADC_CHANNEL_6, GPIO_NUM_1, false, 2400, 0),
+        // Sensor(5, 1300, adc1_handle, ADC_CHANNEL_7, GPIO_NUM_21, false, 300, 0),
+        // Sensor(6, 1300, adc1_handle, ADC_CHANNEL_9, GPIO_NUM_47, true, 360, 0),
+        // Sensor(7, 1300, adc1_handle, ADC_CHANNEL_8, GPIO_NUM_48, false, 420, 0),
+        // Sensor(8, 1300, adc1_handle, ADC_CHANNEL_2, GPIO_NUM_45, true, 480, 0)
     };
 
     auto queue = xQueueCreate(5, sizeof(SamplerQueueItem));
@@ -270,7 +310,7 @@ extern "C" void app_main(void)
     xTaskCreate(sampler, "sampler", 2048, &sampler_params, 5, nullptr);
 
     // Create serial task
-    auto serial_tx_queue = xQueueCreate(50, sizeof(char[MAX_MESSAGE_LEN]));
+    auto serial_tx_queue = xQueueCreate(10, sizeof(char[MAX_MESSAGE_LEN]));
     auto serial_rx_queue = xQueueCreate(10, sizeof(char[MAX_MESSAGE_LEN]));
 
     SerialTaskParams serial_params = {.tx_queue=serial_tx_queue, .rx_queue=serial_rx_queue};
@@ -281,15 +321,22 @@ extern "C" void app_main(void)
     std::queue<SamplerQueueItem> delay_buffer;
 
     // the time between top and bottom sensors, should be calibrated
-    size_t calibration_time_ms = 2100;
-    size_t num_delayed_samples = calibration_time_ms * SAMPLE_RATE / 1000;
-
+    size_t calibration_time_ms = 9400;
     MeasuremenstState measurements_state;
+
+    int64_t calibration_start_timestamp = 0;
+    bool calibrated = false;
+
+    ESP_LOGI(TAG, "Starting...\n");
 
     while(true)
     {
+        size_t num_delayed_samples = calibration_time_ms * SAMPLE_RATE / 1000;
+
         // Hier wordt gebruik gemaakt van een while loop, omdat we alle inkomende samples willen verwerken, als we dit niet doen loopt de queue over
-        while (xQueueReceive(queue, &sampler_queue_item, pdMS_TO_TICKS(1))) {
+        while (xQueueReceive(queue, &sampler_queue_item, pdMS_TO_TICKS(1)))
+        {
+            ESP_LOGI(TAG, "in while loop in main\n");
             // Voeg elk sample wat binnenkomt ook toe aan de delay buffer, hierdoor kunnen we later dynamisch nog de plaatsing van de sensoren veranderen
             delay_buffer.push(sampler_queue_item);
 
@@ -300,10 +347,12 @@ extern "C" void app_main(void)
                 auto delayed_queue_item = delay_buffer.front();
                 delay_buffer.pop();
 
+                // ESP_LOGI(TAG, "delay sample time: %lld, sample time: %lld, delta: %lld", delayed_queue_item.timestamp, sampler_queue_item.timestamp, sampler_queue_item.timestamp-delayed_queue_item.timestamp);
+
                 // printf("item timestamp: %lld - delayed item timestamp: %lld | DELTA: %lld\n", sampler_queue_item.timestamp, delayed_queue_item.timestamp, (sampler_queue_item.timestamp - delayed_queue_item.timestamp)/1000);
 
-                const SensorResult* left_sensor = nullptr;
-                const SensorResult* right_sensor = nullptr;
+                const SensorResult* leftmost_sensor = nullptr;
+                const SensorResult* rightmost_sensor = nullptr;
 
                 for (size_t i = 0; i < NUM_SENSORS; i++)
                 {
@@ -311,25 +360,27 @@ extern "C" void app_main(void)
                         ? delayed_queue_item.results[i] 
                         : sampler_queue_item.results[i];
 
+
                     if (result.connected && result.value >= sensors[i].get_threshold())
                     {
+                        ESP_LOGI(TAG, "Sensor S0%d detected metal, %d", result.id, result.value);
                         // printf("sensor id: %d, detected value: %d\n", result.id, result.value);
-                        if (!left_sensor || sensors[i].get_x() < sensors[left_sensor->id - 1].get_x()) {
-                            left_sensor = &result;
+                        if (!leftmost_sensor || sensors[i].get_x() < sensors[leftmost_sensor->id - 1].get_x()) {
+                            leftmost_sensor = &result;
                         }
 
-                        if (!right_sensor || sensors[i].get_x() > sensors[right_sensor->id - 1].get_x()) {
-                            right_sensor = &result;
+                        if (!rightmost_sensor || sensors[i].get_x() > sensors[rightmost_sensor->id - 1].get_x()) {
+                            rightmost_sensor = &result;
                         }
                     }
                 }
 
                 // Bereken breedte als beide sensoren zijn gevonden
-                if (left_sensor && right_sensor)
+                if (leftmost_sensor && rightmost_sensor)
                 {
-                    int width = sensors[right_sensor->id - 1].get_x() - sensors[left_sensor->id - 1].get_x() + SENSOR_WIDTH;
-                    printf("Breedte: %d mm (sensor %d tot %d)\n",
-                        width, left_sensor->id, right_sensor->id);
+                    int width = sensors[rightmost_sensor->id - 1].get_x() - sensors[leftmost_sensor->id - 1].get_x() + SENSOR_WIDTH;
+                    // printf("Breedte: %d mm (sensor %d tot %d)\n",
+                    //     width, leftmost_sensor->id, rightmost_sensor->id);
 
                     measurements_state.update(sampler_queue_item.timestamp, width);
                 } else
@@ -341,7 +392,16 @@ extern "C" void app_main(void)
                         // TODO!: Config check for [0] sensor
                         int64_t delta =  sampler_queue_item.timestamp - timestamp;
 
-                        printf("Got result - timestamp: %lld, width: %d\n", delta, width);
+                        char payload[MAX_MESSAGE_LEN];
+
+                        snprintf(payload, sizeof(payload), "W=%d:L=%lld",
+                                    width, delta);
+
+                        send_message(serial_tx_queue, "MET", payload);
+
+                        int length = delta / calibration_time_ms;
+
+                        printf("Got result - timestamp: %lld, length: %dmm, width: %dmm\n", delta, length, width);
                     }
 
                     measurements_state.clear();
@@ -353,23 +413,67 @@ extern "C" void app_main(void)
             }
 
    
+            // ESP_LOGI(TAG, "sending measurements at %lld. SENSOR 1 connected: %d", sampler_queue_item.timestamp, sampler_queue_item.results[0].connected);
             send_sensor_measurements(serial_tx_queue, sampler_queue_item);
                 // printf("S0%d - timestamp: %lld, connected: %d, sampled: %d\n", i+1, sampler_queue_item.results[i].timestamp, sampler_queue_item.results[i].connected, sampler_queue_item.results[i].value);        
         }
 
         if (xQueueReceive(serial_rx_queue, &rx_buf, pdMS_TO_TICKS(1)))
         {
-            // printf("Queue receive RX: %s\n", rx_buf);
+            ESP_LOGD(TAG, "Queue receive RX: %s\n", rx_buf);
 
-            // char *token;
+            char *token;
             
-            // /* get the first token */
-            // token = strtok(rx_buf, ":");
+            /* get the first token */
+            token = strtok(rx_buf, ":");
 
-            // while(token != nullptr) {
-            //     printf("token: '%s' \n", token);
-            //     token = strtok(nullptr, ":");
-            // }
+                ESP_LOGI(TAG, "%s", token);
+
+
+            while(token != nullptr) {
+                if (strcmp(token, "CAL") == 0) {
+                    ESP_LOGI(TAG, "CALIBRATION MODE\n");
+
+                    calibrated = false;
+                    calibration_start_timestamp = 0;
+
+                    while(!calibrated)
+                    {
+                        while (xQueueReceive(queue, &sampler_queue_item, pdMS_TO_TICKS(1)))
+                        {
+                            // ESP_LOGI(TAG, "timestamp sample: %lld", sampler_queue_item.timestamp);
+
+                            for (size_t i = 0; i < NUM_SENSORS; i++)
+                            {
+                                if (sampler_queue_item.results[i].connected && sampler_queue_item.results[i].value >= sensors[i].get_threshold())
+                                {
+                                    if (sensors[i].is_delayed() && calibration_start_timestamp == 0)
+                                    {
+                                        calibration_start_timestamp = sampler_queue_item.timestamp;
+                                        ESP_LOGI(TAG, "First calibration timestamp: %lld", calibration_start_timestamp);
+                                    }
+                                    
+                                    // TODO!
+                                    
+                                    if (!sensors[i].is_delayed() && calibration_start_timestamp != 0) {
+                                        calibration_time_ms = sampler_queue_item.timestamp - calibration_start_timestamp;
+                                        calibration_start_timestamp = 0;
+
+                                        ESP_LOGI(TAG, "Calibrated: %f", calibration_time_ms*10e-6);
+                                        calibrated = true;
+                                    }
+                                }
+                            }
+                        }
+
+                        vTaskDelay(pdMS_TO_TICKS(10));
+                    }
+                } else {
+                    ESP_LOGI(TAG, "Unknown token: %s\n", token);
+                }
+
+                token = strtok(nullptr, ":");
+            }
         }
 
         vTaskDelay(pdMS_TO_TICKS(10));
