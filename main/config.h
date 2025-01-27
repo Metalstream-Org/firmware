345842934bd03e8f3093db912c263e8a25c4017c
Martijn Faber <martijn@fabtronics.nl>
2025-01-27 10:03:21 +0000

    feat(config): verplaats constants to config file

--- main/config.h
+++ main/config.h
@@ -5,6 +5,18 @@
 #include "driver/gpio.h"
 
 const size_t NUM_SENSORS = 8;
+// mm
 const size_t SENSOR_WIDTH = 100;
 // mm
 const size_t SENSOR_LONGITUDINAL_SAPCING = 100;
+
+// De grootte van de USB FIFO buffer
+const size_t USB_FIFO_BUF_SIZE = 128;
+// De maximale lengte van een bericht
+const size_t MAX_MESSAGE_LEN = 128;
+// Aantal keer dat we per seconde willen samplen
+const size_t SAMPLE_RATE = 20;
+// Sample interval
+const size_t SAMPLE_INTERVAL_MS = 1000 / SAMPLE_RATE;
+
+const float INITIAL_CONVEYER_BELT_SPEED = 9.1;
